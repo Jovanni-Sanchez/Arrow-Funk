49fee99b3df963166b60ecdc67c82d842e94f943
Jovanni <jovannis8@nycstudents.net>
2024-12-02 20:24:05 -0500

    Arrow Funk Soundtray + Better Notesplash

--- source/backend/FunkinSoundTray.hx
+++ source/backend/FunkinSoundTray.hx
@@ -19,22 +19,20 @@ import flixel.system.ui.FlxSoundTray;
  *  Wouldnt say this is the best way of implemeting this, but it works.
  *  Also Supports The Mods Folders
  */
-class FunkinSoundTray extends FlxSoundTray
-{
+class FunkinSoundTray extends FlxSoundTray {
 	var graphicScale:Float = 0.30;
 	var lerpYPos:Float = 0;
 	var alphaTarget:Float = 0;
 
-	var volumeMaxSound:String;
-
-	public function new()
-	{
+	public function new() {
 		// calls super, then removes all children to add our own
 		// graphics
 		super();
 		removeChildren();
 
-		var bg:Bitmap = new Bitmap(getPath("images/soundtray/volumebox", IMAGE));
+		var bg:Bitmap = new Bitmap(getPath("images/soundtray/backgroundVolume", IMAGE));
+		bg.x = 10;
+		bg.y = 5;
 		bg.scaleX = graphicScale;
 		bg.scaleY = graphicScale;
 		addChild(bg);
@@ -43,13 +41,12 @@ class FunkinSoundTray extends FlxSoundTray
 		visible = false;
 
 		// makes an alpha'd version of all the bars (bar_10.png)
-		var backingBar:Bitmap = new Bitmap(getPath('images/soundtray/bars_10', IMAGE));
-		backingBar.x = 9;
+		var backingBar:Bitmap = new Bitmap(getPath('images/soundtray/mute', IMAGE));
+		backingBar.x = 10;
 		backingBar.y = 5;
 		backingBar.scaleX = graphicScale;
 		backingBar.scaleY = graphicScale;
 		addChild(backingBar);
-		backingBar.alpha = 0.4;
 
 		// clear the bars array entirely, it was initialized
 		// in the super class
@@ -57,10 +54,9 @@ class FunkinSoundTray extends FlxSoundTray
 
 		// 1...11 due to how block named the assets,
 		// we are trying to get assets bars_1-10
-		for (i in 1...11)
-		{
+		for (i in 1...11) {
 			var bar:Bitmap = new Bitmap(getPath('images/soundtray/bars_$i', IMAGE), false);
-			bar.x = 9;
+			bar.x = 10;
 			bar.y = 5;
 			bar.scaleX = graphicScale;
 			bar.scaleY = graphicScale;
@@ -71,16 +67,13 @@ class FunkinSoundTray extends FlxSoundTray
 		y = -height;
 		screenCenter();
 
-		volumeUpSound = 'Volup';
-		volumeDownSound = 'Voldown';
-		volumeMaxSound = 'VolMAX';
+		volumeUpSound = 'volume-up';
+		volumeDownSound = 'volume-down';
 	}
 
-	function getPath(path:String, ?TYPE:AssetType = IMAGE):Dynamic
-	{
+	function getPath(path:String, ?TYPE:AssetType = IMAGE):Dynamic {
 		var ext = '';
-		switch (TYPE)
-		{
+		switch (TYPE) {
 			case IMAGE:
 				ext = 'png';
 				var file = Paths.getPath('$path.$ext', IMAGE);
@@ -101,32 +94,26 @@ class FunkinSoundTray extends FlxSoundTray
 		return null;
 	}
 
-	override public function update(MS:Float):Void
-	{
+	override public function update(MS:Float):Void {
 		y = CoolUtil.coolLerp(y, lerpYPos, 0.1);
 		alpha = CoolUtil.coolLerp(alpha, alphaTarget, 0.25);
 
 		// Animate sound tray thing
-		if (_timer > 0)
-		{
+		if (_timer > 0) {
 			_timer -= (MS / 1000);
 			alphaTarget = 1;
-		}
-		else if (y >= -height)
-		{
+		} else if (y >= -height) {
 			lerpYPos = -height - 10;
 			alphaTarget = 0;
 		}
 
-		if (y <= -height)
-		{
+		if (y <= -height) {
 			visible = false;
 			active = false;
 
 			#if FLX_SAVE
 			// Save sound preferences
-			if (FlxG.save.isBound)
-			{
+			if (FlxG.save.isBound) {
 				FlxG.save.data.mute = FlxG.sound.muted;
 				FlxG.save.data.volume = FlxG.sound.volume;
 				FlxG.save.flush();
@@ -140,43 +127,33 @@ class FunkinSoundTray extends FlxSoundTray
 	 *
 	 * @param	up Whether the volume is increasing.
 	 */
-	override public function show(up:Bool = false):Void
-	{
+	override public function show(up:Bool = false):Void {
 		_timer = 1;
 		lerpYPos = 10;
 		visible = true;
 		active = true;
 		var globalVolume:Int = Math.round(FlxG.sound.volume * 10);
 
-		if (FlxG.sound.muted)
-		{
+		if (FlxG.sound.muted) {
 			globalVolume = 0;
 		}
 
-		if (!silent)
-		{
+		if (!silent) {
 			var sound = null;
 			#if MODS_ALLOWED
 			sound = Paths.returnSound('sounds/soundtray/${up ? volumeUpSound : volumeDownSound}');
-			#else 
+			#else
 			sound = FlxAssets.getSound(up ? volumeUpSound : volumeDownSound);
 			#end
-			
-			if (globalVolume == 10)
-				sound = Paths.returnSound('sounds/soundtray/$volumeMaxSound');
 
 			if (sound != null)
 				FlxG.sound.load(sound).play();
 		}
 
-		for (i in 0..._bars.length)
-		{
-			if (i < globalVolume)
-			{
+		for (i in 0..._bars.length) {
+			if (i < globalVolume) {
 				_bars[i].visible = true;
-			}
-			else
-			{
+			} else {
 				_bars[i].visible = false;
 			}
 		}
